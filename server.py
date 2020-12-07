c23eb7b8c8335f858c183d6d949b90340b612898
Sergio Gonzalez <sergiomanuel2020@gmail.com>
2020-12-07 09:18:31 -0600

    Create server.py

---
+++ server.py
@@ -0,0 +1,36 @@
+import threading
+import socket
+
+host = "127.0.0.1" #localhost
+port = 555555
+
+server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast (message):
+    for client in clients:
+        client.send(message)
+
+def handle(client):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} left the chat'.encode("ascii"))
+            nicknames.remove(nickname)
+            break
+
+def recive():
+    while  True:
+        client, address = server.accept()
+        print(f"Connectede with {str(address)}")
+        
